d5a667e656c2bf8947e4e9ef6d3021f40590c5aa
Weston <barnesweston@gmail.com>
2022-03-02 12:32:45 -0600

    add css, highscore html, and javascript files

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// start button that, when clicked, starts a timer and presents a question
+
+// when a question is answered, the next one is prompted
+
+// incorrect questions penalize by subtracting from timer 
+
+// game is over when all questions are answered or the timer reaches 0
+// when game is over, save initials and score in highscores page 
